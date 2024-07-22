b029f25e9d44bf6b1739b464a30f78576deae22e
Meta8750 <paul.powietzka@gmail.com>
2024-07-22 15:56:53 +0200

    some changes in design and form

--- React/idle/src/UI/Sidebar.js
+++ React/idle/src/UI/Sidebar.js
@@ -1,14 +1,24 @@
 import React from "react";
-import styles from '../UIcss/Header.module.css'
+import { useState } from "react";
+import styles from '../UIcss/Sidebar.module.css'
+
 
 function Sidebar() {
+    const [activeTab, setActiveTab] = useState();
+
+    
+
+
     return (
         <div className={styles.sidebar}>
             <div>
                 <h1>My Sidebar</h1>
             </div>
-            <ul>
-                <li></li>
+            <ul className={styles.list}>
+                <li>Test1</li>
+                <li>Test2</li>
+                <li>Test3</li>
+                <li>Test4</li>
                 
             </ul>
         </div>

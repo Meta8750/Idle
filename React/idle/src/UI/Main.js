f3e25903d967a2bfdb09e9dfe39898a153b0b823
Meta8750 <paul.powietzka@gmail.com>
2024-07-25 09:31:35 +0200

    bug fix with forgotten var

--- React/idle/src/UI/Main.js
+++ React/idle/src/UI/Main.js
@@ -1,50 +1,33 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
 import styles from '../UIcss/Main.module.css';
-
-import Hub from './Hub.js'
-import Mining from './Mining.js';
+import Mining from './Mining';
 import Player from '../system/Player.js';
 
-
-
-
 function Main({ activeTab }) {
+  const [activity, setActivity] = useState(null);
+  const [time, setTime] = useState(0);
 
-    const [activity, setActivity] = useState(null);
-    const [time, setTime] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-                setTime((prev) => prev + 1);
-        },1000);
-
-        return () => {
-            
-            clearInterval(interval);
-
-            
-            }
-                
-            
-    }, [])
-
-    
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div>
-      
       <div className={activeTab === 'Player' ? styles.visible : styles.hidden}>
         <Player time={time} activity={activity} />
       </div>
       <div className={activeTab === 'Mining' ? styles.visible : styles.hidden}>
-        <Mining setActivity={setActivity}/>
+        <Mining setActivity={setActivity} />
       </div>
-      <p>{activity}</p>
+      
     </div>
   );
-  
-
 }
 
 export default Main;

7847ef4ea3f6dc46bcf9ec7f78d6c74ef440f071
Meta8750 <paul.powietzka@gmail.com>
2024-09-13 14:06:11 +0200

    delete useless construkto

--- src/system/MonDex.ts
+++ src/system/MonDex.ts
@@ -1,9 +1,10 @@
 import Animon from "./Animon.ts";
 import { monData } from "./MonDexData.ts";
 
+//class to generate Animon's 
+//Data from an other file names: "MonDexData"
 export default class MonDex{
-    constructor(){
-    }
+    
     generateMon(id: number){
         const monInfo = monData.find(mon => mon.id === id);
         if (!monInfo) {

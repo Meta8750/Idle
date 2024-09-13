7ddb0517cd851a649aa16c159508df86dd6b7e8d
Meta8750 <paul.powietzka@gmail.com>
2024-09-13 13:31:58 +0200

    material ordner change

--- src/UI/skills/Mining.js
+++ src/UI/skills/Mining.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import styles from '../../UIcss/skills/Mining.module.css';
 import { useState, useEffect } from 'react';
-import Material from './Material.ts';
+import Material from '../../system/Material.ts';
 function Mining({time, player}) {
     const progressBarWidth = player.getCurrentSkill() ? ( time / (player.getCurrentSkill().CD + player.getActivity().additionalCD)) * 100 + '%' : '0%';
     const [ores, setOres] = useState({
       job: "mining",
       type: {
-        stone :     new Material("stone", 1, 1, 1, 0, 0,10,"Mining"),
-        copper:     new Material("copper", 1, 1, 1, 0, 0,10,"Mining"),
-        tin:        new Material("tin", 1, 1, 1, 0, 0,10,"Mining"), 
-        cole:       new Material("cole", 1, 1, 1, 0, 0,10,"Mining"),
+        stone :     new Material("stone",       1, 1, 1, 0, 0,10,"Mining"),
+        copper:     new Material("copper",      1, 2, 3, 1, 0.5,10,"Mining"),
+        tin:        new Material("tin",         1, 5, 1, 5, 0,10,"Mining"), 
+        cole:       new Material("cole",        2, 10, 1, 10, 0,10,"Mining"),
   
-        iron:       new Material("iron", 1, 1, 1, 0, 0,10,"Mining"),
-        gold:       new Material("gold", 1, 1, 1, 0, 0,10,"Mining"),  
-        emerald:    new Material("emerald", 1, 1, 1, 0, 0,10,"Mining"),
-        crystal:    new Material("crystal", 1, 1, 1, 0, 0,10,"Mining"),
+        iron:       new Material("iron",        1, 20, 1, 15, 0,10,"Mining"),
+        gold:       new Material("gold",        1, 44, 1, 20, 0,10,"Mining"),  
+        emerald:    new Material("emerald",     1, 57, 1, 35, 0,10,"Mining"),
+        crystal:    new Material("crystal",     1, 150, 1, 40, 0,10,"Mining"),
   
-        beryll:     new Material("beryll", 1, 1, 1, 0, 0,10,"Mining"),
-        azurit:     new Material("azurit", 1, 1, 1, 0, 0,10,"Mining"), 
-        pyrit:      new Material("pyrit", 1, 1, 1, 0, 0,10,"Mining"),
-        uranini:    new Material("uranini", 1, 1, 1, 0, 0,10,"Mining"),
+        beryll:     new Material("beryll",      1, 220, 1, 45, 0,10,"Mining"),
+        azurit:     new Material("azurit",      1, 270, 1, 50, 0,10,"Mining"), 
+        pyrit:      new Material("pyrit",       1, 350, 1, 55, 0,10,"Mining"),
+        uranini:    new Material("uranini",     1, 410, 1, 60, 0,10,"Mining"),
         
-        celestial:  new Material("celestial", 1, 1, 1, 0, 0,10,"Mining"),
-        astral:     new Material("astral", 1, 1, 1, 0, 0,10,"Mining"),
-        dragonite:  new Material("dragonite", 1, 1, 1, 0, 0,10,"Mining"),
-        darkmatter: new Material("darkmatter", 1, 1, 1, 1, 0,10,"Mining"),
+        celestial:  new Material("celestial",   1, 1, 1, 65, 0,10,"Mining"),
+        astral:     new Material("astral",      1, 1, 1, 70, 0,10,"Mining"),
+        dragonite:  new Material("dragonite",   1, 1, 1, 80, 0,10,"Mining"),
+        darkmatter: new Material("darkmatter",  1, 1, 1, 90, 0,10,"Mining"),
       
       }
     });
@@ -62,7 +62,7 @@ function Mining({time, player}) {
                       <p class="px-5">M: {ores.type[ore].mastery}</p>
                       <p>MExp: {ores.type[ore].masteryExp}</p>
 
-                      <p>Min. level: S{ores.type[ore].minLevel}</p>
+                      <p>Min. level: {ores.type[ore].minLevel}</p>
                       <div className={styles.masteryBar}><i className={styles.masteryBarI} style={{ width: ores.type[ore].masteryBarWidth }}></i></div>
                       
                   </div>

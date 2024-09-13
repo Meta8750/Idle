27bbae4c663772f12b753e63ec8217d2edba7237
Meta8750 <paul.powietzka@gmail.com>
2024-09-13 14:37:45 +0200

    monStats ui improvement

--- src/UI/MonStats.js
+++ src/UI/MonStats.js
@@ -9,14 +9,16 @@ function MonStats({mon}){
             <div>
                 <h1>{mon.name}</h1>
                 {mon.getImageElement()}
-                <li>{mon.maxhealth}/{mon.health}</li>
-                <li>{mon.maxMana}/{mon.mana}</li>
-                <li>{mon.baseMS}</li>
+                <li class="text-green-400" >{mon.maxHealth}/{mon.health}</li>
+                <li class="text-blue-300">{mon.maxMana}/{mon.mana}</li>
+                <li class="text-orange-500">AD: {mon.baseAD}</li>
+                <li class="text-blue-600">AP: {mon.baseAP}</li>
+                <li>MS: {mon.baseMS}</li>
                 <div>{mon.attacks.map((attack, index) => {
                     return <li>
                     {attack.name} deals <span className={attack.type === "AP" ? styles.ap : styles.ad}>{Math.floor(attack.baseDMG + (mon.baseAD * attack.adScaling) + (mon.baseAP * attack.apScaling))}
-                    </span> = <span className={styles.ad}> {attack.baseDMG} + {(attack.adScaling * 100)}% 
-                    </span> + <span className={styles.ap}> {attack.baseDMG} + {(attack.apScaling * 100)}%
+                    </span> = <span className={styles.ad}> {attack.baseDMG} + {mon.baseAD * attack.adScaling} ({(attack.adScaling * 100)}%) 
+                    </span> + <span className={styles.ap}> {attack.baseDMG} + {mon.baseAP * attack.apScaling} ({(attack.apScaling * 100)}%)
                     </span></li>
                 })}</div>
                

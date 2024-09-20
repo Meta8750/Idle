90d52cc4b0806d56d6c52d08a71854e9493c0379
Meta8750 <paul.powietzka@gmail.com>
2024-09-20 13:35:28 +0200

    new var

---
+++ src/system/dex/dropTableDex.ts
@@ -0,0 +1,19 @@
+
+export const dropTable = [
+    {
+     id:40000,
+     common: {
+        dropID: 30000,
+        getDrop(){
+             
+             return 1000
+         }
+     },
+     rare: {
+         exp:150
+     },
+     legendary:{
+         exp:200
+     }
+    },
+ ]

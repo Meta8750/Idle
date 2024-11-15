44f92e81f9067566249f98e79ff1fb016eb8d441
Meta8750 <paul.powietzka@gmail.com>
2024-11-15 11:55:55 +0100

    starting page beginning

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import styles from './UIcss/Main.module.css';
 import Sidebar from './UI/Sidebar.js';
 import Header from './UI/Header.js';
 
+import Startpage from './userEntry/Startpage.js'
+
 import Mining from './UI/skills/Mining.js';
 import Gambling from './UI/skills/Gambling.js';
 import Smithing from './UI/skills/Smithing.js';
@@ -26,12 +28,12 @@ import Logout from './system/Logout.js';
 import Casino from './system/Casino.ts';
 
 let flag = false
+let login = false;
 const player = new Player();
 const casino = new Casino();
 function App() {
   const [time, setTime] = useState(0);
-  
- 
+  const [login, setLogin] = useState(false);
 
   useEffect(() => {
     const tick = () => {
@@ -50,59 +52,70 @@ function App() {
   const activeTab = player.getActiveTab()
   flag = player.progress(time)
   
+  if (login) {
+    return (
+      <div className="App">
+        
+        <header> <Header player={player}/> </header>
+        <aside> <Sidebar player={player} setTime={setTime} time={time}/> </aside>
+        <main className={styles.main}> 
+          <div>
+            <div className={activeTab === 'Inventory' ? styles.visible : styles.hidden}>
+              <DisplayInventory  player={player} />
+            </div>
+            <div className={activeTab === 'Shop' ? styles.visible : styles.hidden}>
+              <Shop  player={player} />
+            </div>
+           
+            <div className={activeTab === 'Story' ? styles.visible : styles.hidden}>
+              <Story  time={time} player={player}/>
+            </div>
+  
+            <div className={activeTab === 'Raid' ? styles.visible : styles.hidden}>
+              <Raid  time={time} player={player}/>
+            </div>
+           
+            <div  className={activeTab === 'MonManager' ? styles.visible : styles.hidden}>
+              <MonManager player={player}/>
+            </div>
+            <div  className={activeTab === 'Laboratory' ? styles.visible : styles.hidden}>
+              <Laboratory player={player}/>
+            </div>
+            <div  className={activeTab === 'Summoning' ? styles.visible : styles.hidden}>
+              <Summoning player={player}/>
+            </div>
+            <div className={activeTab === 'Mining' ? styles.visible : styles.hidden}>
+              <Mining  time={time} player={player}/>
+            </div>
+            <div className={activeTab === 'Cutting' ? styles.visible : styles.hidden}>
+              <Cutting  time={time} player={player}/>
+            </div>
+            <div className={activeTab === 'Smithing' ? styles.visible : styles.hidden}>
+              <Smithing  time={time} player={player}/>
+            </div>
+            <div className={activeTab === 'Crafting' ? styles.visible : styles.hidden}>
+              <Crafting  time={time} player={player}/>
+            </div>
+            <div className={activeTab === 'Gambling' ? styles.visible : styles.hidden}>
+              <Gambling player={player} casino={casino}/>
+            </div>
+            <div className={activeTab === 'Logout' ? styles.visible : styles.hidden}>
+              <Logout   time={time} player={player}/>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Startpage />
+        
+      </div>
+    );
+  }
 
-  return (
-    <div className="App">
-      <header> <Header player={player}/> </header>
-      <aside> <Sidebar player={player} setTime={setTime} time={time}/> </aside>
-      <main className={styles.main}> 
-        <div>
-          <div className={activeTab === 'Inventory' ? styles.visible : styles.hidden}>
-            <DisplayInventory  player={player} />
-          </div>
-          <div className={activeTab === 'Shop' ? styles.visible : styles.hidden}>
-            <Shop  player={player} />
-          </div>
-         
-          <div className={activeTab === 'Story' ? styles.visible : styles.hidden}>
-            <Story  time={time} player={player}/>
-          </div>
-
-          <div className={activeTab === 'Raid' ? styles.visible : styles.hidden}>
-            <Raid  time={time} player={player}/>
-          </div>
-         
-          <div  className={activeTab === 'MonManager' ? styles.visible : styles.hidden}>
-            <MonManager player={player}/>
-          </div>
-          <div  className={activeTab === 'Laboratory' ? styles.visible : styles.hidden}>
-            <Laboratory player={player}/>
-          </div>
-          <div  className={activeTab === 'Summoning' ? styles.visible : styles.hidden}>
-            <Summoning player={player}/>
-          </div>
-          <div className={activeTab === 'Mining' ? styles.visible : styles.hidden}>
-            <Mining  time={time} player={player}/>
-          </div>
-          <div className={activeTab === 'Cutting' ? styles.visible : styles.hidden}>
-            <Cutting  time={time} player={player}/>
-          </div>
-          <div className={activeTab === 'Smithing' ? styles.visible : styles.hidden}>
-            <Smithing  time={time} player={player}/>
-          </div>
-          <div className={activeTab === 'Crafting' ? styles.visible : styles.hidden}>
-            <Crafting  time={time} player={player}/>
-          </div>
-          <div className={activeTab === 'Gambling' ? styles.visible : styles.hidden}>
-            <Gambling player={player} casino={casino}/>
-          </div>
-          <div className={activeTab === 'Logout' ? styles.visible : styles.hidden}>
-            <Logout   time={time} player={player}/>
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+  
 }
 
 export default App;
---
+++ src/userEntry/Startpage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+// import styles from '../UIcss/S.module.css'
+
+
+function Startpage() {
+    
+    return (
+       <div>
+        <header>
+            <div>
+
+            </div>
+        </header>
+        
+        <aside></aside>
+        
+        <main></main>
+        
+        <footer></footer>
+       
+       </div>
+    )
+}
+
+export default Startpage;
